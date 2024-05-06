2f7f823ccf2994fe9fc5e2e652909a3465649e1b
Admin <camthinguyen@gmail.com>
2024-05-06 11:46:06 +0700

    Add headless option

--- src/test/java/saucedemo/com/testcases/BaseTest.java
+++ src/test/java/saucedemo/com/testcases/BaseTest.java
@@ -1,25 +1,31 @@
 package saucedemo.com.testcases;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-
 
 public class BaseTest {
 	public static WebDriver driver;
 	public static final String URL_login = "https://www.saucedemo.com/";
-	
-    @BeforeAll
+
+	@BeforeAll
 	public void setUp() {
-    	WebDriverManager.firefoxdriver().browserVersion("124.0.2").setup();
-    	driver  = new FirefoxDriver();
+		WebDriverManager.firefoxdriver().browserVersion("124.0.2").setup();
+		boolean headlessMode = Boolean.parseBoolean(System.getProperty("headless"));
+		FirefoxOptions options = new FirefoxOptions();
+		
+		if (headlessMode == true)
+			options.addArguments("-headless");
+		driver = new FirefoxDriver(options);
 		driver.get(URL_login);
 	}
 
 	@AfterAll
-	public void tearDown() {		
+	public void tearDown() {
 		driver.quit();
 	}
 }
